bf4835eaf5a08c81ff60543624747da15c15e343
Joselito <josalbarr@alu.edu.gva.es>
2024-10-28 21:54:42 +0100

    doin' things

--- weridoSpanishRitual/app/Http/Requests/ProfileUpdateRequest.php
+++ weridoSpanishRitual/app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['nullable', 'string', 'max:255'],
+            'birthday' => ['nullable', 'date', 'after_or_equal:1950-01-01', 'before:today'],
             'email' => [
                 'required',
                 'string',
--- weridoSpanishRitual/database/seeders/DatabaseSeeder.php
+++ weridoSpanishRitual/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create(["name" => "Mr Babu", 'surname' => null, "email" => "a@a.a", "password" => "a", 'birthday' => null, 'isAdmin' => "is"]);
-        User::factory()->create(["name" => "Martiluski07", 'surname' => "Stinky with no PC", "email" => "b@b.b", "password" => "b", 'birthday' => null, 'isAdmin' => "isNot"]);
+        User::factory()->create(["name" => "Martiluski07", 'surname' => "Stinky that doesn't have PC", "email" => "b@b.b", "password" => "b", 'birthday' => null, 'isAdmin' => "isNot"]);
     }
 }
