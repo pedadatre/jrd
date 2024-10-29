2a00a1c6b4a37f991fc51c2e36bfbdc0926c01a8
Jose <akstarlaantiguaestrellabeta@gmail.com>
2024-10-29 20:17:58 +0100

    xzcvb

--- weridoSpanishRitual/resources/views/layouts/navigation.blade.php
+++ weridoSpanishRitual/resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
@@ -23,6 +23,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <!--Put here a if else to detect the admin-->
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
--- weridoSpanishRitual/routes/web.php
+++ weridoSpanishRitual/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,7 +9,8 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $usr_data = User::all();
+    return view('dashboard', compact('usr_data'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
